111444faec947952df09d39f99fb2d18bdfbacd3
farid-parmarketing <farid.ansari@parmarketing.agency>
2025-02-05 17:01:46 +0530

    Auth

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,147 +15,137 @@ import { FaFileInvoiceDollar } from "react-icons/fa";
 import { GiPublicSpeaker } from "react-icons/gi";
 import { FaLocationArrow } from "react-icons/fa";
 import { AppContext } from "../context/AppContext";
-import Loader from "../components/Loader";
 
 const Home = () => {
-  const { lengths, loading, user } = useContext(AppContext);
+  const { lengths, user } = useContext(AppContext);
   //
   return (
     <>
       <div className="container">
-        {loading ? (
-          <>
-            <Loader />
-            <p className="text-center">
-              Please hold on. This might take few minutes.
-            </p>
-          </>
-        ) : (
-          <>
-            {lengths !== null && (
-              <div className="home-grid">
-                <Link to="/basicdetails" className="box">
-                  <div className="title">
-                    <p>Business details</p>
-                    <FaBriefcase />
-                  </div>
-                  <div className="description">
-                    <p>{user.Company}</p>
-                  </div>
-                </Link>
-
-                <div
-                  className="box upload-box"
-                  data-bs-toggle="modal"
-                  data-bs-target="#uploadInvoice"
-                >
-                  <p>Upload Invoice</p>
-                  <MdCloudUpload />
+        <>
+          {user !== null && (
+            <div className="home-grid">
+              <Link to="/basicdetails" className="box">
+                <div className="title">
+                  <p>Business details</p>
+                  <FaBriefcase />
                 </div>
+                <div className="description">
+                  <p>{user.Company}</p>
+                </div>
+              </Link>
 
-                <Link to="/existingdebtor" className="box">
-                  <div className="title">
-                    <p>No of debtors</p>
-                    <MdCreditCardOff />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.noOfDebtors}</p>
-                  </div>
-                </Link>
-
-                <Link to="/invoicelist" className="box">
-                  <div className="title">
-                    <p>No of invoices</p>
-                    <FaFileInvoice />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.noOfInvoices}</p>
-                  </div>
-                </Link>
-
-                <Link to="/cashcollected" className="box">
-                  <div className="title">
-                    <p>Cash collected</p>
-                    <FaMoneyBillAlt />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.cashCollected}</p>
-                  </div>
-                </Link>
-
-                <Link to="/promisetopay" className="box">
-                  <div className="title">
-                    <p>Promise to pay</p>
-                    <FaMoneyBill />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.promiseTopay}</p>
-                  </div>
-                </Link>
-
-                <Link to="/meritsofcases" className="box">
-                  <div className="title">
-                    <p>Merits of cases</p>
-                    <FaSuitcase />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.meritsOfCases}</p>
-                  </div>
-                </Link>
-
-                <Link to="/legalactions" className="box">
-                  <div className="title">
-                    <p>Legal actions</p>
-                    <GoLaw />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.legalActions}</p>
-                  </div>
-                </Link>
-
-                <Link to="/abscondedcases" className="box">
-                  <div className="title">
-                    <p>Absconded cases</p>
-                    <RiCalendarCloseLine />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.abscondedCases}</p>
-                  </div>
-                </Link>
-
-                <Link to="/liveinvoices" className="box">
-                  <div className="title">
-                    <p>Live invoices</p>
-                    <FaFileInvoiceDollar />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.liveInvoices}</p>
-                  </div>
-                </Link>
-
-                <Link to="/disputedinvoices" className="box">
-                  <div className="title">
-                    <p>Disputed invoices</p>
-                    <GiPublicSpeaker />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.disputedInvoices}</p>
-                  </div>
-                </Link>
-
-                <Link to="/fieldvisits" className="box">
-                  <div className="title">
-                    <p>Field visits</p>
-                    <FaLocationArrow />
-                  </div>
-                  <div className="description">
-                    <p>{lengths.fieldVisits}</p>
-                  </div>
-                </Link>
+              <div
+                className="box upload-box"
+                data-bs-toggle="modal"
+                data-bs-target="#uploadInvoice"
+              >
+                <p>Upload Invoice</p>
+                <MdCloudUpload />
               </div>
-            )}
-          </>
-        )}
+
+              <Link to="/existingdebtor" className="box">
+                <div className="title">
+                  <p>No of debtors</p>
+                  <MdCreditCardOff />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.noOfDebtors}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/invoicelist" className="box">
+                <div className="title">
+                  <p>No of invoices</p>
+                  <FaFileInvoice />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.noOfInvoices}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/cashcollected" className="box">
+                <div className="title">
+                  <p>Cash collected</p>
+                  <FaMoneyBillAlt />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.cashCollected}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/promisetopay" className="box">
+                <div className="title">
+                  <p>Promise to pay</p>
+                  <FaMoneyBill />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.promiseTopay}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/meritsofcases" className="box">
+                <div className="title">
+                  <p>Merits of cases</p>
+                  <FaSuitcase />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.meritsOfCases}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/legalactions" className="box">
+                <div className="title">
+                  <p>Legal actions</p>
+                  <GoLaw />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.legalActions}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/abscondedcases" className="box">
+                <div className="title">
+                  <p>Absconded cases</p>
+                  <RiCalendarCloseLine />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.abscondedCases}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/liveinvoices" className="box">
+                <div className="title">
+                  <p>Live invoices</p>
+                  <FaFileInvoiceDollar />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.liveInvoices}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/disputedinvoices" className="box">
+                <div className="title">
+                  <p>Disputed invoices</p>
+                  <GiPublicSpeaker />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.disputedInvoices}</p> */}0
+                </div>
+              </Link>
+
+              <Link to="/fieldvisits" className="box">
+                <div className="title">
+                  <p>Field visits</p>
+                  <FaLocationArrow />
+                </div>
+                <div className="description">
+                  {/* <p>{lengths.fieldVisits}</p> */}0
+                </div>
+              </Link>
+            </div>
+          )}
+        </>
       </div>
 
       {/* upload invoice modal */}
--- frontend/src/utility/Auth.jsx
+++ frontend/src/utility/Auth.jsx
@@ -1,16 +1,58 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const Auth = ({ Component }) => {
-  const { getUser } = useContext(AppContext);
+  const { url, generateToken, setUser } = useContext(AppContext);
+  const navigate = useNavigate();
+  //
+  const [loading, setLoading] = useState(true);
+  //
+  const getUser = async () => {
+    try {
+      const id = localStorage.getItem("tc-portal-id");
+      if (!id) {
+        navigate("/login", { replace: true });
+      } else {
+        const token = await generateToken();
+        const res = await axios.get(
+          `${url}/proxy?url=https://www.zohoapis.in/crm/v2/Leads/${JSON.parse(
+            id
+          )}`,
+          {
+            headers: { Authorization: `Zoho-oauthtoken ${token}` },
+          }
+        );
+        let userData = res.data.data[0];
+        for (let item in userData) {
+          if (userData[item] === null) {
+            userData[item] = "";
+          }
+        }
+        setUser(userData);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
     getUser();
   }, []);
   return (
     <>
-      <Navbar />
-      <Component />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Component />
+        </>
+      )}
     </>
   );
 };
